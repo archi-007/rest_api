07e8d8d87230b8d80a27d1754a0a5e0ca31a455d
archi-007 <archiribhu@gmail.com>
2020-03-29 22:38:29 +0530

    added serializers

--- core/views.py
+++ core/views.py
@@ -6,13 +6,21 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializers import PostSerializer
+from .models import Post
+
 
 class TestView(APIView):
 
     def get(self, request, *args, **kwargs):
-        data = {
-            'name' : 'Archi',
-            'age' : 23,
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many = True)
+        return Response(serializer.data)
 
-        }
-        return Response(data)
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data = request.data)
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors)
